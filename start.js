c5df88ad0aa5ee38d0d01567846a8ea136f58272
Jim McDermott <jpmcde@gmail.com>
2019-04-20 18:24:04 -0400

    with programmatic forever

---
+++ start.js
@@ -0,0 +1,8 @@
+var forever = require('forever-monitor');
+var child = new (forever.Monitor)('index.js', {
+  max: 3,
+  silent: true,
+  args: []
+});
+child.on('exit', function () {console.log('your-filename.js has exited after 3 restarts');});
+child.start();
