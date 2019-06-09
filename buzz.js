64a5524f52f093a8755a86fdfeb2ad2391fcaeb1
Jim McDermott <jpmcde@gmail.com>
2019-06-09 15:48:51 -0400

    updated gpio numbers added toggleBlue sub

--- buzz.js
+++ buzz.js
@@ -4,14 +4,14 @@ var blynk=new Blynk.Blynk(AUTH,options={connector:new Blynk.TcpClient()});
 
 var v1=new blynk.VirtualPin(1);
 const{exec}=require('child_process');
-exec('gpio mode 15 out');
-exec('gpio mode 4 out');
-exec('gpio write 4 1');
+exec('gpio mode 15 out');exec('gpio mode 7 out');
+toggleBlue();setTimeout(function(){toggleBlue();},2000);
 
+function toggleBlue(){exec('gpio toggle 7');}
 function turnOff(){exec('gpio write 15 0');}
 turnOff();
 
 v1.on('write',function(param){
-  exec('gpio write 15 1');
+  exec('gpio write 15 1');console.log('time');
   setTimeout(function(){turnOff();},3000);
 });
